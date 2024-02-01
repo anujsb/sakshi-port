08541742087f664766c453ded4816d24b8407bf2
anuj <anujsbhuyar@gmail.com>
2024-02-01 10:50:44 +0530

    navbar

--- client/components/main/Navbar.tsx
+++ client/components/main/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a
+        {/* <a
           href="#about-me"
           className="h-auto w-auto flex flex-row items-center"
         >
@@ -21,7 +21,7 @@ const Navbar = () => {
           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
             WebChain Dev
           </span>
-        </a>
+        </a> */}
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
